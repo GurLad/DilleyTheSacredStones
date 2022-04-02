c3f8d357e9c5011d05fef0cc9d100dee50f908db
Gur Lad <40135047+GurLad@users.noreply.github.com>
2022-04-02 15:00:56 +0300

    Added a basic game controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static readonly Vector2Int MAX_COORDS = new Vector2Int(1, 1);
+    private static readonly Vector2Int MIN_COORDS = new Vector2Int(-1, -1);
+
+    public float ForwardSpeed;
+    public float MoveSpeed;
+    public float RotationSpeed;
+    public float ShipSize;
+    public Transform ShipObject;
+    private Vector2Int coords = Vector2Int.zero;
+    // Move vars
+    private Vector2Int movingToCoords = MAX_COORDS + Vector2Int.right;
+    private bool moving { get => movingToCoords.x <= MAX_COORDS.x; }
+    private float count;
+    private Vector2Int lastInput;
+
+    private void Update()
+    {
+        Vector2Int targetMove = new Vector2Int(Control.GetAxisIntDown(Control.Axis.X), Control.GetAxisIntDown(Control.Axis.Y));
+        if (!moving)
+        {
+            if (targetMove != Vector2Int.zero || (targetMove = lastInput) != Vector2Int.zero)
+            {
+                Debug.Log("Target move: " + targetMove);
+                Vector2Int targetPos = coords + targetMove;
+                targetPos.Clamp(MIN_COORDS, MAX_COORDS);
+                Debug.Log("Target pos: " + targetPos);
+                if (targetPos != coords)
+                {
+                    movingToCoords = targetPos;
+                    count = 0;
+                }
+            }
+            lastInput = Vector2Int.zero;
+        }
+        else
+        {
+            if (targetMove != Vector2Int.zero)
+            {
+                lastInput = targetMove;
+            }
+            count += Time.deltaTime * MoveSpeed;
+            if (count >= 1)
+            {
+                count = 1;
+            }
+            ShipObject.transform.localPosition = new Vector3(ShipSize * (coords.x * (1 - count) + movingToCoords.x * count), ShipSize * (coords.y * (1 - count) + movingToCoords.y * count));
+            Vector2Int diff = coords - movingToCoords;
+            ShipObject.localEulerAngles = new Vector3(diff.y * Mathf.Sin(count * Mathf.PI), -diff.x * Mathf.Sin(count * Mathf.PI)) * RotationSpeed;
+            if (count >= 1)
+            {
+                coords = movingToCoords;
+                movingToCoords = MAX_COORDS + Vector2Int.right;
+                count = 0;
+            }
+        }
+    }
+}
